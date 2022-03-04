80a20a93fa2f9a8775666742033c7f022e51b3ea
Gil Tayar <gil@tayar.org>
2022-03-04 15:25:12 +0200

    03-import-package-directly is working

---
+++ 03-import-package-directly/src/03-import-package-directly.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line n/no-unpublished-import
+import {makeCounterComponent} from '../node_modules/a-bundleless-future-counter-component/src/a-bundleless-future-counter-component.js'
+
+makeCounterComponent(document.querySelector('#counter-1'))
+makeCounterComponent(document.querySelector('#counter-2'))
