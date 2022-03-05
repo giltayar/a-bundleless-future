abb3bb3ab4db50ba04789bf222db0c8c8278029d
Gil Tayar <gil@tayar.org>
2022-03-05 16:55:07 +0200

    06-importmaps-cdn working
    [bilt-with-bilt]

---
+++ 06-importmaps-prod-cdn/src/06-importmaps-prod-cdn.js
@@ -0,0 +1,4 @@
+import {makeCounterComponent} from 'a-bundleless-future-counter-component'
+
+makeCounterComponent(document.querySelector('#counter-1'))
+makeCounterComponent(document.querySelector('#counter-2'))
