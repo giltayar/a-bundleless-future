4351ec912a877f915d4db22446c1b0af79dd0652
Gil Tayar <gil@tayar.org>
2022-03-19 10:18:14 +0200

    03 update dependencies
    [bilt-with-bilt]

--- 03-import-package-directly/src/03-import-package-directly.js
+++ 03-import-package-directly/src/03-import-package-directly.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line n/no-unpublished-import
-import {makeCounterComponent} from '../node_modules/a-bundleless-future-counter-component/src/a-bundleless-future-counter-component.js'
+import {makeCounterComponent} from '../node_modules/a-bundleless-future-counter-component/src/index.js'
 
 makeCounterComponent(document.querySelector('#counter-1'))
 makeCounterComponent(document.querySelector('#counter-2'))
